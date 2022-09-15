ddd33e0c502827dcad8d2b4edf4001d327822c92
ZakharchukA <zaharchyk83@gmail.com>
2022-09-15 14:47:23 +0300

    finished section-2

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,11 @@ $(document).ready(function(){
         loop:true,
         margin:0,
         nav:true,
+        // autoplay:true,
+        autoplayTimeout:6000,
+        autoplayHoverPause:true,
+        smartSpeed: 800,
+        lazyLoad:true,
         navText: [ '', ' ' ],
 
         responsive:{
